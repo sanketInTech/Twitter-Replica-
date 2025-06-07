d496527d9e671fa69aaa4facf159f3fd482911e2
sanketInTech <dsanket034@gmail.com>
2025-06-07 23:08:36 +0530

    fix: PrismaClient initialization error on Vercel

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,7 +4,10 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prismadb = global.prisma || new PrismaClient()
+const prismadb = global.prisma || new PrismaClient({
+  log: ['query', 'error', 'warn'], // Log queries, errors, and warnings
+  errorFormat: 'pretty', // Make errors more readable
+})
 
 if (process.env.NODE_ENV === 'development') global.prisma = prismadb
 
